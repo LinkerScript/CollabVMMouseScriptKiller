2d8fd597c8749c72175e2590fa89321ed9b4b100
Diary <tributionllc@gmail.com>
2020-12-12 04:24:19 +0000

    Add name changing feature

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import webbot
 import time
 from webbot import Browser
+import json
 
 # Add a way to import the driver
 web = Browser()
@@ -18,8 +19,14 @@ web.click("Windows 10 Enterprise 1909 x64 (VM 5)")
 # Sleep and wait for the page to load
 time.sleep(3)
 
+# Change username to "Collab VM Mouse Script Killer"
+web.click("Change Username")
+web.type("Collab VM Mouse Script Killer")
+web.click("OK")
+
 # Click on "Take Turn" and wait 10 seconds then click on it again.
 while True:
   print("Clicked 'Take Turn. Waiting 8s.....")
   web.click("Take Turn")
   time.sleep(8)
+  
